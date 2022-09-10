c6a20838d25bc3d31b3a220bbbd8247e396b55bf
AmiteshVeeragattam <vdnamitesh@gmail.com>
2022-09-10 14:24:22 +0530

    add blog page

--- myportfolio/views.py
+++ myportfolio/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'myportfolio/html/index.html')
+
+def blog(request):
+    return render(request, 'myportfolio/html/blog.html')
